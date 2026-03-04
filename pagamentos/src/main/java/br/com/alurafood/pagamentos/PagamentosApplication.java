b3c43063629a4c7d6eeae755b7f9834101e6f526
SidneySilvaAraujo <sidneiaraujo219@gmail.com>
2026-03-04 15:00:38 -0300

    adcionando feignclient

--- pagamentos/src/main/java/br/com/alurafood/pagamentos/PagamentosApplication.java
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/PagamentosApplication.java
@@ -2,12 +2,14 @@ package br.com.alurafood.pagamentos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class PagamentosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PagamentosApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PagamentosApplication.class, args);
+    }
 
 }
