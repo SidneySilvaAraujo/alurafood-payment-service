e82b13920034f7082f3889cf29c20b0f0be74e68
SidneySilvaAraujo <sidneiaraujo219@gmail.com>
2026-03-18 16:10:12 -0300

    adcionando metodo fallback

--- pagamentos/src/main/java/br/com/alurafood/pagamentos/controller/PagamentoController.java
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/controller/PagamentoController.java
@@ -2,6 +2,7 @@ package br.com.alurafood.pagamentos.controller;
 
 import br.com.alurafood.pagamentos.dto.PagamentoDto;
 import br.com.alurafood.pagamentos.service.PagamentoService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -56,8 +57,13 @@ public class PagamentoController {
     }
 
     @PatchMapping("/{id}/confirmar")
+    @CircuitBreaker(name = "atualizaPedido", fallbackMethod = "pagamentoConfirmadoComIntegracaoPendente")
     public void confirmarPagamento(@PathVariable @NotNull Long id) {
         service.confirmarPagamento(id);
+    }
+
+    public void pagamentoConfirmadoComIntegracaoPendente(Long id, Exception e) {
+        service.alteraStatus(id);
     }
 }
 
